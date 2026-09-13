5844a6a5e73031bfd4c9a991e030ef8c3d60652e
mehedi259 <mmridul207@gmail.com>
2026-09-13 08:50:58 +0600

    Add name to UserChatSerializer

--- chat/serializers.py
+++ chat/serializers.py
@@ -5,7 +5,7 @@ from users.models import User
 class UserChatSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'phone', 'avatar']
+        fields = ['id', 'first_name', 'last_name', 'name', 'phone', 'avatar']
 
 class MessageSerializer(serializers.ModelSerializer):
     sender_id = serializers.IntegerField(source='sender.id', read_only=True)
